0e3ed97c330c2625cd0f4b9570ce87c51b89817b
ziplus112 <ne.ehsan@outlook.com>
2026-09-24 02:52:06 +0330

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,1 @@
+import type { CapacitorConfig } from '@capacitor/cli'; const config: CapacitorConfig={appId:'com.ehsan.neumorphictimer',appName:'Neumorphic Timer',webDir:'www',bundledWebRuntime:false,server:{androidScheme:'https'}}; export default config;
